b98d128f268af75de62f8f20d6de70ed66d9a730
Anıl TOPUZ <aniltopuz@Anls-MacBook-Pro.local>
2022-10-21 00:05:33 +0300

    update

--- finans-app.py
+++ finans-app.py
@@ -1,4 +1,5 @@
 
+from cgi import test
 from distutils.log import info
 from itertools import count
 from bs4 import BeautifulSoup
@@ -36,26 +37,31 @@ for i in href:
 
 data_info_dict = {}
 data_list={}
+text={}
+data_name={}
 data_n=[]
 for url in link_list:
+
+
        page=requests.get(url)
        soup = BeautifulSoup(page.content, 'html.parser')
        name = soup.find("h2")
        print(name.text)
        div =soup.find('div',class_='p-3')
        span = div.findAll('ul')
-       data_info_dict["name"]=name.text  
        for i in span:
           li = i.findAll('li')
           for j in li:
                 span =j.find('span') #hisse bilgi adları
                 tag= j.select_one(":nth-child(2)") #hisse bilgi degerleri
-                data_info_dict[span.text]=tag.text
-       text = {
-          "name":name.text,
-          "data":data_info_dict
-           }
-       data_n.append(text)
+                text={
+                    "name":name.text,
+                     span.text:tag.text
+                }
+          data_n.append(text)
+
+        
+     
 
        
        with open("data-info.json", "w",encoding='utf-8') as f:
